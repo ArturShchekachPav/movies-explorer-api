42a1efff986dedf8f968719cd2a91538c662ade7
ArturShchekachPav <arturshchekatch@yandex.ru>
2023-08-09 23:52:29 +1000

    refactor: change default port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const centralErrorHandler = require('./middlewares/central-error-handler');
 require('dotenv').config();
 
 
-const { PORT = 3005, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
